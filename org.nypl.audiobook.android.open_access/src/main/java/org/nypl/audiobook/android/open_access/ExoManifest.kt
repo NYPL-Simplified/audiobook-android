fc16dc8e8640be619d20f0006e6bb2be17e61cdb
Mark Raynsford <code@io7m.com>
2018-07-25 10:04:59 +0000

    Complete manifest parsing
    Add unit tests.

--- org.nypl.audiobook.android.open_access/src/main/java/org/nypl/audiobook/android/open_access/ExoManifest.kt
+++ org.nypl.audiobook.android.open_access/src/main/java/org/nypl/audiobook/android/open_access/ExoManifest.kt
@@ -1,6 +1,9 @@
 package org.nypl.audiobook.android.open_access
 
 import org.nypl.audiobook.android.api.PlayerManifest
+import org.nypl.audiobook.android.api.PlayerManifestScalar
+import org.nypl.audiobook.android.api.PlayerManifestScalar.PlayerManifestScalarNumber
+import org.nypl.audiobook.android.api.PlayerManifestSpineItem
 import org.nypl.audiobook.android.api.PlayerResult
 import java.net.URI
 
@@ -14,8 +17,82 @@ data class ExoManifest(
   val spineItems: List<ExoManifestSpineItem>) {
 
   companion object {
+
+    /**
+     * Parse an ExoPlayer manifest from the given raw manifest.
+     */
+
     fun transform(manifest: PlayerManifest): PlayerResult<ExoManifest, Exception> {
-      return PlayerResult.Failure(Exception("Not implemented!"))
+      try {
+        return PlayerResult.Success(ExoManifest(
+          manifest.metadata.title,
+          manifest.metadata.identifier,
+          manifest.spine.mapIndexed { index, item -> processSpineItem(index, item) }))
+      } catch (e: Exception) {
+        return PlayerResult.Failure(e)
+      }
+    }
+
+    private fun processSpineItem(
+      index: Int,
+      item: PlayerManifestSpineItem): ExoManifestSpineItem {
+
+      val values = item.values
+      val title = parseTitle(values, index)
+      val type = parseType(values)
+      val uri = parseURI(values, index)
+      val duration = parseDuration(values, index)
+
+      return ExoManifestSpineItem(
+        title = title,
+        part = 0,
+        chapter = index,
+        type = type,
+        duration = duration,
+        uri = uri)
+    }
+
+    private fun parseDuration(values: Map<String, PlayerManifestScalar>, index: Int): Double {
+      if (values.containsKey("duration")) {
+        val value = values["duration"]
+        return when(value) {
+          is PlayerManifestScalar.PlayerManifestScalarString ->
+            throw IllegalArgumentException("Spine item ${index} has an invalid 'duration' field")
+          is PlayerManifestScalar.PlayerManifestScalarBoolean ->
+            throw IllegalArgumentException("Spine item ${index} has an invalid 'duration' field")
+          is PlayerManifestScalarNumber.PlayerManifestScalarReal ->
+            value.number
+          is PlayerManifestScalarNumber.PlayerManifestScalarInteger ->
+            value.number.toDouble()
+          null ->
+            throw IllegalArgumentException(
+              "Spine item ${index} is missing the required 'duration' field")
+        }
+      }
+      throw IllegalArgumentException("Spine item ${index} is missing the required 'duration' field")
+    }
+
+    private fun parseURI(values: Map<String, PlayerManifestScalar>, index: Int): URI {
+      if (values.containsKey("href")) {
+        return URI(values["href"].toString())
+      }
+      throw IllegalArgumentException("Spine item ${index} is missing the required 'href' field")
+    }
+
+    private fun parseType(values: Map<String, PlayerManifestScalar>): String {
+      return if (values.containsKey("type")) {
+        return values["type"].toString()
+      } else {
+        "application/octet-stream"
+      }
+    }
+
+    private fun parseTitle(values: Map<String, PlayerManifestScalar>, index: Int): String {
+      return if (values.containsKey("title")) {
+        values["title"].toString()
+      } else {
+        index.toString()
+      }
     }
   }
 }
