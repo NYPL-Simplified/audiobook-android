2b225cd1876526d4d92baf8746e421be4f0e18ca
Mark Raynsford <code@io7m.com>
2018-10-12 19:39:01 +0000

    Improve TOC aesthetics
    This reimplements the TOC to align with the latest NYPL
    designs.
    
      1. Remove the text from the circular progress indicators, and use
         a grey background for the ring (so that a grey ring indicates 0%
         progress).
      2. Add an icon when there's an error.
      3. Remove the ability to delete individual parts.
      4. Add a small "refresh" icon used to download parts.
    
    Testing: All unit tests pass.
    
    Affects: https://jira.nypl.org/browse/SIMPLY-1299

--- org.nypl.audiobook.android.views/src/main/java/org/nypl/audiobook/android/views/PlayerTOCAdapter.kt
+++ org.nypl.audiobook.android.views/src/main/java/org/nypl/audiobook/android/views/PlayerTOCAdapter.kt
@@ -3,8 +3,6 @@ package org.nypl.audiobook.android.views
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
-import android.graphics.Color
-import android.graphics.ColorFilter
 import android.graphics.PorterDuff
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -78,6 +76,8 @@ class PlayerTOCAdapter(
     val status = item.downloadStatus
     when (status) {
       is PlayerSpineElementNotDownloaded -> {
+        holder.durationText.visibility = VISIBLE
+
         if (!item.book.supportsStreaming) {
           holder.view.setBackgroundColor(holder.backgroundDisabled)
           holder.durationText.setTextColor(holder.textColorDisabled)
@@ -88,63 +88,69 @@ class PlayerTOCAdapter(
           holder.titleText.setTextColor(holder.textColorNormal)
         }
 
+        holder.errorIcon.visibility = INVISIBLE
+
         holder.operationButton.visibility = VISIBLE
-        holder.operationButton.setImageResource(R.drawable.download)
+        holder.operationButton.setImageResource(R.drawable.refresh)
         holder.operationButton.setOnClickListener({ item.downloadTask.fetch() })
 
         holder.downloadProgress.setOnClickListener({ })
         holder.downloadProgress.visibility = INVISIBLE
         holder.downloadProgress.progress = 0.0f
-        holder.downloadProgressText.visibility = INVISIBLE
       }
 
       is PlayerSpineElementDownloading -> {
+        holder.durationText.visibility = INVISIBLE
+
         if (!item.book.supportsStreaming) {
           holder.view.setBackgroundColor(holder.backgroundDisabled)
-          holder.durationText.setTextColor(holder.textColorDisabled)
           holder.titleText.setTextColor(holder.textColorDisabled)
         } else {
           holder.view.setBackgroundColor(holder.backgroundColorNormal)
-          holder.durationText.setTextColor(holder.textColorNormal)
           holder.titleText.setTextColor(holder.textColorNormal)
         }
 
+        holder.errorIcon.visibility = INVISIBLE
         holder.operationButton.visibility = INVISIBLE
         holder.operationButton.setOnClickListener({ })
 
         holder.downloadProgress.setOnClickListener({ this.onConfirmCancelDownloading(item) })
         holder.downloadProgress.visibility = VISIBLE
         holder.downloadProgress.progress = status.percent.toFloat() * 0.01f
-        holder.downloadProgressText.visibility = VISIBLE
-        holder.downloadProgressText.text = status.percent.toString()
       }
 
       is PlayerSpineElementDownloaded -> {
+        holder.durationText.visibility = VISIBLE
+
         holder.view.setBackgroundColor(holder.backgroundColorNormal)
         holder.durationText.setTextColor(holder.textColorNormal)
         holder.titleText.setTextColor(holder.textColorNormal)
 
-        holder.operationButton.visibility = VISIBLE
-        holder.operationButton.setImageResource(R.drawable.trash)
-        holder.operationButton.setOnClickListener({ this.onConfirmDelete(item) })
+        holder.errorIcon.visibility = INVISIBLE
+        holder.operationButton.visibility = INVISIBLE
 
         holder.downloadProgress.setOnClickListener({ })
         holder.downloadProgress.visibility = INVISIBLE
-        holder.downloadProgressText.visibility = INVISIBLE
       }
 
       is PlayerSpineElementDownloadFailed -> {
+        holder.durationText.visibility = INVISIBLE
+
         holder.view.setBackgroundColor(holder.backgroundColorNormal)
         holder.durationText.setTextColor(holder.textColorNormal)
         holder.titleText.setTextColor(holder.textColorNormal)
 
+        holder.errorIcon.visibility = VISIBLE
+
         holder.operationButton.visibility = VISIBLE
-        holder.operationButton.setImageResource(R.drawable.error)
-        holder.operationButton.setOnClickListener({ item.downloadTask.delete() })
+        holder.operationButton.setImageResource(R.drawable.refresh)
+        holder.operationButton.setOnClickListener({
+          item.downloadTask.delete()
+          item.downloadTask.fetch()
+        })
 
         holder.downloadProgress.setOnClickListener({ })
         holder.downloadProgress.visibility = INVISIBLE
-        holder.downloadProgressText.visibility = INVISIBLE
       }
     }
 
@@ -153,25 +159,10 @@ class PlayerTOCAdapter(
     view.setOnClickListener(this@PlayerTOCAdapter.listener)
 
     if (position == this.currentSpineElement) {
-      holder.border.visibility = VISIBLE
+      holder.isCurrent.visibility = VISIBLE
     } else {
-      holder.border.visibility = INVISIBLE
+      holder.isCurrent.visibility = INVISIBLE
     }
-  }
-
-  private fun onConfirmDelete(item: PlayerSpineElementType) {
-    val dialog =
-      AlertDialog.Builder(this.context)
-        .setCancelable(true)
-        .setMessage(R.string.audiobook_part_delete_confirm)
-        .setPositiveButton(
-          R.string.audiobook_part_delete,
-          { _: DialogInterface, _: Int -> item.downloadTask.delete() })
-        .setNegativeButton(
-          R.string.audiobook_part_delete_keep,
-          { _: DialogInterface, _: Int -> })
-        .create()
-    dialog.show()
   }
 
   private fun onConfirmCancelDownloading(item: PlayerSpineElementType) {
@@ -216,14 +207,14 @@ class PlayerTOCAdapter(
 
     val titleText: TextView =
       this.view.findViewById(R.id.player_toc_item_view_title)
-    val border: ImageView =
-      this.view.findViewById(R.id.player_toc_item_view_border)
+    val isCurrent: ImageView =
+      this.view.findViewById(R.id.player_toc_item_is_current)
+    val errorIcon: ImageView =
+      this.view.findViewById(R.id.player_toc_item_error)
     val durationText: TextView =
       this.view.findViewById(R.id.player_toc_item_view_duration)
     val downloadProgress: PlayerCircularProgressView =
       this.view.findViewById(R.id.player_toc_item_view_progress)
-    val downloadProgressText: TextView =
-      this.view.findViewById(R.id.player_toc_item_view_progress_text)
     val operationButton: ImageView =
       this.view.findViewById(R.id.player_toc_item_view_operation)
 
@@ -231,7 +222,11 @@ class PlayerTOCAdapter(
       this.downloadProgress.thickness = 8.0f
       this.downloadProgress.color = this@PlayerTOCAdapter.parameters.primaryColor
 
-      this.border.setColorFilter(
+      this.errorIcon.setColorFilter(
+        this@PlayerTOCAdapter.parameters.primaryColor, PorterDuff.Mode.MULTIPLY)
+      this.operationButton.setColorFilter(
+        this@PlayerTOCAdapter.parameters.primaryColor, PorterDuff.Mode.MULTIPLY)
+      this.isCurrent.setColorFilter(
         this@PlayerTOCAdapter.parameters.primaryColor, PorterDuff.Mode.MULTIPLY)
     }
   }
