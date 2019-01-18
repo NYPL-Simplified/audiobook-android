eaf31c6ab7d611b34a39bc0b87de087619914d65
Mark Raynsford <code@io7m.com>
2019-01-18 22:06:06 +0000

    Allow downloading via RBDigital link documents
    This adds the required support for downloading book parts via RBDigital
    JSON link documents.
    
    Affects: https://jira.nypl.org/browse/SIMPLY-1627

---
+++ org.nypl.audiobook.android.rbdigital/src/main/java/org/nypl/audiobook/rbdigital/RBDigitalLinkDocument.kt
@@ -0,0 +1,21 @@
+package org.nypl.audiobook.rbdigital
+
+import java.net.URI
+
+/**
+ * An indirect link to another file.
+ */
+
+data class RBDigitalLinkDocument(
+
+  /**
+   * The MIME type of the target.
+   */
+
+  val type: String,
+
+  /**
+   * The URI of the target.
+   */
+
+  val uri: URI)
